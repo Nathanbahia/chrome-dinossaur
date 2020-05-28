999a21057dc8fd7bb37eb6da9e15b1e2d9c7b21e
nathanbahia <51969733+Nathanbahia@users.noreply.github.com>
2020-05-28 13:01:55 -0300

    Add files via upload

--- game.py
+++ game.py
@@ -1,38 +1,46 @@
-### CREATED BY: NATHAN BAHIA - @NOOBPYTHONBR ###
-### SOUNDTRACK: WYS - SNOWMAN
-### PIXEL ART MADE WITH PISKELAPP.COM
-
 import os
 import pygame
 from random import randint, choice
-
 pygame.init()
 
-tela = pygame.display.set_mode((500, 300))
+
+tela = pygame.display.set_mode((800, 480))
 pygame.display.set_caption("@noobpythonbr")
+
 clock = pygame.time.Clock()
+background = pygame.image.load(os.path.join("images/background.png"))
 
 score = 0
-font = pygame.font.SysFont("System", 18, bold=True, italic=False)
+font = pygame.font.SysFont("System", 22, bold=True, italic=False)
 
 class Dino:
 	def __init__(self):
 		self.image = pygame.image.load(os.path.join("images/dino.png"))
+		# definição das dimensões do sprite
 		self.sprite_x = 0
 		self.sprite_w = 32
 		self.sprite_h = 35
+		self.sprite = self.image.subsurface((self.sprite_x, 0), (self.sprite_w, self.sprite_h))
+		# definição da velocidade de transição dos sprites
 		self.sprite_count = 0
 		self.sprite_speed = 5
-		self.sprite = self.image.subsurface((self.sprite_x, 0), (self.sprite_w, self.sprite_h))
+		
+		# definições da área de colisão e posicionamento
 		self.rect = self.sprite.get_rect()
 		self.rect.x, self.rect.y = 100, 210
+      	# self.rect tem 4 atributos [ x (posição no eixo X), y (posição no eixo Y), w (largura), h (altura) ]
 
+		# definições do sistema de pulo
 		self.jump_speed = 5
 		self.jump_force = 50
 		self.weight = 1
 		self.is_jumping = True
 		
 	def update(self):
+		'''
+		Animação dos sprites
+		'''
+      
 		if self.sprite_count == self.sprite_speed:
 			if self.sprite_x < self.image.get_size()[0] - (self.rect.w + 1):
 				self.sprite_x += self.sprite_w
@@ -41,27 +49,36 @@ class Dino:
 			self.sprite_count = 0
 		self.sprite_count += 1
 		
-		
+		'''
+		Sitema de pulo.
+		'''
+              
 		if self.is_jumping == True:
 			if self.jump_speed > 0:
 				F = ( 0.5 * self.weight * (self.jump_speed * self.jump_speed))
 			else:
-				F = -( 0.5 * self.weight * (self.jump_speed * self.jump_speed))			
-			self.rect.y = self.rect.y - F
-						
+				F = -( 0.5 * self.weight * (self.jump_speed * self.jump_speed))			              
+			self.rect.y = self.rect.y - F						
 			self.jump_speed -=  1
 
-			if self.rect.y >= 210:
-				self.rect.y = 210
+			if self.rect.y >= 370:
+				self.rect.y = 370
 				self.is_jumping = False
-				self.jump_speed = 8 			
+				self.jump_speed = 8 					
 		
-		self.jump()
-		
+		'''
+		Plotagem do sprite na tela.
+		'''
+              
 		self.sprite = self.image.subsurface((self.sprite_x, 0), (self.sprite_w, self.sprite_h))
 		tela.blit(self.sprite, (self.rect.x, self.rect.y))
 		
 	def jump(self):
+		'''
+		Verifica o pressionamento da tecla espaço e checa se o dino já não está pulando,
+		então, troca o status de self.is_jumping para verdadeiro.
+		'''
+
 		key = pygame.key.get_pressed()
 		if key[pygame.K_SPACE]:
 			if self.is_jumping == False:
@@ -69,82 +86,94 @@ class Dino:
 				
 				
 	def check_collision(self, element):
-		self.element = element
-		if self.rect.colliderect(self.element.rect):
-			pygame.time.delay(1500)
-			self.element.rect.x = 600
-			global score
-			score = 0
-		
-		
+		'''
+		Verifica a colisão do dino com os obstáculos.
+		'''	
+      
+		global score
+		self.element = element # grupo de elementos que podem colidir com o dino
+		for e in self.element:
+			if self.rect.colliderect(e.rect):
+				pygame.time.delay(1500)
+				e.rect.x -= 100			
+				score = 0
+
 class Cactus:
-        def __init__(self):		
-                self.image = pygame.image.load(os.path.join("images/cactus.png"))
-                self.sprite_x = choice([0, 32])
-                self.sprite_y = 0
-                self.sprite = self.image.subsurface((self.sprite_x, self.sprite_y),(32, 32))
-                self.rect = self.sprite.get_rect()
-                self.rect.x, self.rect.y = 1000, 210
-                self.speed = 8
+	def __init__(self):		
+		self.image = pygame.image.load(os.path.join("images/cactus.png"))
+		self.sprite_x = choice([0, 32])
+		self.sprite_y = 0
+		self.sprite = self.image.subsurface((self.sprite_x, self.sprite_y),(32, 32))
+		self.rect = self.sprite.get_rect()
+		self.rect.x, self.rect.y = 1000, 370
+		self.speed = 8
 
+	def update(self):                                
+		if score < 50:
+			self.sprite_y = 0
+		elif score < 100:                
+			self.sprite_y = 64
+		else:            
+			self.sprite_y = 32
 
-        def update(self):
-
-                global score
-                if score < 50:
-                        tela.blit(pygame.image.load(os.path.join("images/bg1.png")), (0, 0))
-                elif 50 < score < 100:
-                        tela.blit(pygame.image.load(os.path.join("images/bg2.png")), (0, 0))
-                        self.sprite_y = 32
-                else:
-                        tela.blit(pygame.image.load(os.path.join("images/bg3.png")), (0, 0))
-                        self.sprite_y = 64
-
-                if self.rect.x + self.rect.w < 0:			
-                        self.rect.x = randint(500, 700)
-                        self.sprite_x = choice([0, 32])
-                self.sprite = self.image.subsurface((self.sprite_x, self.sprite_y),(32, 32))
-                self.rect.x -= self.speed				
-                tela.blit(self.sprite, (self.rect.x, self.rect.y))                        
+		if self.rect.x + self.rect.w < 0:			
+			self.rect.x = randint(800, 900)
+			self.sprite_x = choice([0, 32])
+			self.sprite = self.image.subsurface((self.sprite_x, self.sprite_y),(32, 32))
+		self.rect.x -= self.speed				
+		tela.blit(self.sprite, (self.rect.x, self.rect.y))
 
 
 class Clouds:
-        def __init__(self):
-                self.image = pygame.image.load(os.path.join("images/cloud.png"))                                
-                self.rect = self.image.get_rect()
-                self.rect.x, self.rect.y = 50, randint(0, 130)
+    def __init__(self):
+        self.image = pygame.image.load(os.path.join("images/cloud.png"))                                
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = 50, randint(0, 270)
 
 
-        def update(self):							
-                if self.rect.x + self.rect.w < 0:
-                        self.rect.x = 500
-                        self.rect.y = randint(0, 130)			                                                
-                self.rect.x -= 1						
-                tela.blit(self.image, (self.rect.x, self.rect.y))
-
+    def update(self):							
+        if self.rect.x + self.rect.w < 0:
+            self.rect.x = 800
+            self.rect.y = randint(0, 270)			                                                
+        self.rect.x -= 1						
+        tela.blit(self.image, (self.rect.x, self.rect.y))
 
 									
-cactus = Cactus()				
+cactus1 = Cactus()				
+cactus2 = Cactus(); cactus2.rect.x = 500
+cactus = [cactus1, cactus2]
+
 dino = Dino()
 
-c1 = Clouds()
-c2 = Clouds(); c2.rect.x = 200
-c3 = Clouds(); c3.rect.x = 400
-clouds = [c1, c2, c3]
+nuvem1 = Clouds()
+nuvem2 = Clouds(); nuvem2.rect.x = 200
+nuvem3 = Clouds(); nuvem3.rect.x = 400
+nuvem4 = Clouds(); nuvem4.rect.x = 560
+nuvem5 = Clouds(); nuvem5.rect.x = 780
+nuvens = [nuvem1, nuvem2, nuvem3, nuvem4, nuvem5]
 
 while True:
-        for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                        exit()
-               
-        cactus.update()
-        dino.update()
-        dino.check_collision(cactus)
-        [c.update() for c in clouds]
-        
-        score += 0.1
-        s = font.render(str(int(score)), True, (0,0,0), None)
-        tela.blit(s, (450, 10))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+    
+    if score < 100:
+        tela.blit(background.subsurface((0,0),(800,480)), (0, 0))
+    elif score < 300:
+        tela.blit(background.subsurface((800,0),(800,480)), (0, 0))
+    else:
+        tela.blit(background.subsurface((1600,0),(800,480)), (0, 0))
+    
+    [n.update() for n in nuvens]
+    [c.update() for c in cactus]    
 
-        clock.tick(30)
-        pygame.display.update()
+    dino.update()
+    dino.check_collision(cactus)
+    dino.jump()
+        
+    score += 0.1
+    s = font.render(str(int(score)), True, (0,0,0), None)
+    tela.blit(s, (700, 10))
+
+    clock.tick(30)
+    pygame.display.update()
